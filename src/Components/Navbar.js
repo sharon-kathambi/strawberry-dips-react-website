fb07d7d14338a646f272fc63180e0ad6359a7f06
Sharon Mwenda <smkathambi@gmail.com>
2022-07-26 15:19:04 +0300

    state hook for the menu icon

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,9 +1,17 @@
-import React from 'react';
-import { ReactDOM } from 'react-dom';
+import React, { useState } from 'react';
+//import { ReactDOM } from 'react-dom';
 import { Link } from 'react-router-dom';
 
 
 function Navbar() {
+
+const [click, setClick] = useState(false);
+
+function handleClick () {
+    setClick(!click)
+}
+
+
   return (
     <>
      <nav className='navbar'>
@@ -11,9 +19,15 @@ function Navbar() {
        <Link to="/" className='navbar-logo'>
         Strawberry Dips  <i className='fab fa-typo3' />
         </Link> 
-        <div className='menu-icon'> 
+        <div className='menu-icon' onClick={handleClick}> 
         <i className={click ? "fas fa-times" : "fas fa-bars"} />
         </div> 
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li>
+                
+            </li>
+
+        </ul>
 
     </div>    
     </nav> 
